7ee8be6c4358e0fbe5ec87a013ea8b52fde2c1a4
Seddon <283481855@qq.com>
2021-12-10 19:36:47 +0800

    首页页面整好了，现在在加路由

--- login/admin.py
+++ login/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
+from .models import User
 
 
-admin.site.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        (None, {'fields': ['sex', 'email', 'c_time', 'has_confirmed']})
+    ]
+    list_display = (['name', 'sex', 'email', 'c_time', 'has_confirmed'])  # add more columns.
+    search_fields = ['name']  # add search function.
+
+
+admin.site.register(User, UserAdmin)
+# admin.site.register(models.User)
 admin.site.register(models.ConfirmString)
-
