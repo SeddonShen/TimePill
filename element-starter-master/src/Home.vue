ce8bbc9c3e7d1d85dd0df3f62aec33511f97b4b9
Seddon <283481855@qq.com>
2021-12-12 14:41:00 +0800

    快修好了
    其实就没有坏

--- element-starter-master/src/Home.vue
+++ element-starter-master/src/Home.vue
@@ -6,29 +6,24 @@
 </template>
 
 <script>
+	import axios from 'axios'
+	import Qs from 'qs'
+	axios.defaults.withCredentials = true; //让ajax携带cookie
     export default {
         data(){
             return {}
         },
 		created:function(){
 			console.log("NB")
+			axios.get("http://localhost:8000/articles/",'').then(
+			  function (resp) {
+				var ses = window.sessionStorage
+				console.log(resp)
+				const result = resp.data.request
+				// const flag = resp.data.request['flag']
+
+			  }
+			)
 		},
-		axios.post("http://localhost:8000/login/",data).then(
-		  function (resp) {
-			var ses = window.sessionStorage
-			console.log(resp)
-			const result = resp.data.request
-			// const flag = resp.data.request['flag']
-			if (result == 'Login ok'){
-			  // console.log(resp.data.request['flag'])
-			  _this.$router.push("/home")
-			}else if(result == 'Have Login' ){
-				alert(result)
-				_this.$router.push("/home")
-			}else {
-			  alert(result)
-			}
-		  }
-		)
     }
 </script>
